8a947e3d30f159d58e8d05995dde0a1917b018c6
= <=>
2021-03-18 08:51:07 -0400

    Solved first quetions

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -1,5 +1,7 @@
 package math.problems;
 
+import java.util.Scanner;
+
 public class Factorial {
 
     /*
@@ -8,8 +10,37 @@ public class Factorial {
      Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120
      */
 
+//    public static void main(String[] args) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter the number: ");
+//
+//        int number = scanner.nextInt();
+//        int factorialOfNumber = factorial(number);
+//        System.out.println("Factorial of this number is " + factorialOfNumber);
+//    }
+//    static int factorial(int n){
+//        int outputResult;
+//        if(n==1){
+//            return 1;
+//        }
+//        outputResult = factorial(n-1) * n;
+//        return outputResult;
+//    }
+
+
+    //
+    public static long factorial(int f){
+        long facNum = 1;
+
+        for (int i = 1; i <= f; i++){
+            facNum = facNum * i;
+        }
+        return facNum;
+    }
+
     public static void main(String[] args) {
-
-
+        int f = 5;
+        System.out.println("Factorial of " +f+ " is " + factorial(f));
     }
 }
