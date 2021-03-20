b57bf1dce7a4bd27bf39e51f60e60898cb114162
= <=>
2021-03-20 08:38:04 -0400

    Solved some quetions

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,5 +1,7 @@
 package string.problems;
 
+import java.util.Arrays;
+
 public class Anagram {
 
     /*
@@ -8,7 +10,30 @@ public class Anagram {
         Example: "CAT" and "ACT", "ARMY" and "MARY", "FART" and "RAFT"
     */
 
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
+            String str1="Cat";
+            String str2="Act";
+
+            str1= str1.toLowerCase();
+            str2=str2.toLowerCase();
+
+            if (str1.length()==str2.length()){
+
+                char[] charArray1= str1.toCharArray();
+                char[] charArray2= str2.toCharArray();
+
+                Arrays.sort(charArray1);
+                Arrays.sort(charArray2);
+                boolean result=Arrays.equals(charArray1,charArray2);
+
+                if (result){
+                    System.out.println(str1+" and "+str2+ " are anagrams.");
+                }
+                else {
+                    System.out.println(str1+" and "+str2+" :are not anagrams.");
+                }
+            }
 
     }
 }
