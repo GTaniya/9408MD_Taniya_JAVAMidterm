d833671fea2eb69db4993bd56af38dc8ffb146d6
= <=>
2021-03-21 00:49:54 -0400

    Solved

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,5 +1,8 @@
 package string.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateWord {
 
     public static void main(String[] args) {
@@ -10,7 +13,29 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        st = st.toLowerCase();
+
+        String[] word = st.split("\\s");
+        Map<String, Integer> mp = new HashMap<String, Integer>();
+        int count = 0;
+
+        for (int i = 0; i < word.length; i++) {
+            count = 1;
+
+            for (int j = 1; j < word.length; j++) {
+                if (word[i].equals(word[j])) {
+                    count++;
+
+                    word[j] = "0";
+                }
+            }
+
+              mp.put(word[i], count);
+            if (count > 1 && word[i] != "0")
+                System.out.println(mp);
+
+
+        }
 
     }
-
 }
